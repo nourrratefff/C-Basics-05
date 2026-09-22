e4fd99a3671321d54f5e81890ded7f7078c3e0fd
Nour Atef <Nour Atef@LAPTOP-2M74GP4J>
2026-09-22 04:46:15 +0300

    Sheet 4: Question 1

--- C#Basics-05.Answers/Program.cs
+++ C#Basics-05.Answers/Program.cs
@@ -4,9 +4,13 @@
     {
         static void Main(string[] args)
         {
+            #region Sheet 04: Question 1 
+
+            double[] prices = { 25.5, 40.0, 33.75};
+
+            #endregion
 
 
-            
         }
     }
 }
